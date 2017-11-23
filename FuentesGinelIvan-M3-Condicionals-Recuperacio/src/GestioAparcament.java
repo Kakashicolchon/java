0e7f6e3c82b59078c37e7b578a227c5aac07af7b
jitsu <jitsu@KakashicolchonYT>
2017-11-23 13:54:39 +0100

    Pedir datos

--- FuentesGinelIvan-M3-Condicionals-Recuperacio/src/GestioAparcament.java
+++ FuentesGinelIvan-M3-Condicionals-Recuperacio/src/GestioAparcament.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 
 public class GestioAparcament {
 
 	public static void main(String[] args) {
-	int holaquease;
-	holaquease = 1;
+	//preu de l'estada segons temps d'estacionament, longitud de vehicles i 
+	// antiguitat dels clients
+		double tempsEstacionament = 0; 
+		double longitudVehicle = 0;
+		int antiguitatClients;
+		
+		Scanner s = new Scanner (System.in);
+		do {
+			System.out.print("Quant de temps estaràs estacionat (Minuts): ");
+			tempsEstacionament = s.nextDouble();
+			System.out.print("Quina es la longitud del teu vehicle (Metres: ");
+			longitudVehicle = s.nextDouble();
+			System.out.print("Quant portes venint a aquest aparcament?(Anys, xifra entera): ");
+			antiguitatClients = s.nextInt();
+			if (tempsEstacionament > longitudVehicle || tempsEstacionament  <= 0) {
+				System.out.println("No heu introduït els valors correctes.");
+				System.out.println("Recodeu que el 0 tampoc és un valor acceptat");
+				
+			}
+		} while (tempsEstacionament > longitudVehicle || tempsEstacionament  <= 0);
+		s.close();
 	}
 
 }
