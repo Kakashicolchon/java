cb5b70fed92ff5fab100499421dce76042b29a75
jitsu <jitsu@KakashicolchonYT>
2017-11-23 15:57:07 +0100

    update GestionAparacmiento

--- FuentesGinelIvan-M3-Condicionals-Recuperacio/src/GestioAparcament.java
+++ FuentesGinelIvan-M3-Condicionals-Recuperacio/src/GestioAparcament.java
@@ -3,27 +3,33 @@ import java.util.Scanner;
 public class GestioAparcament {
 
 	public static void main(String[] args) {
-	//preu de l'estada segons temps d'estacionament, longitud de vehicles i 
-	// antiguitat dels clients
-		double tempsEstacionament = 0; 
+		//preu de l'estada segons temps d'estacionament, longitud de vehicles i 
+		// antiguitat dels clients
+		int tempsEstacionament = 0; 
 		double longitudVehicle = 0;
-		int antiguitatClients;
-		
+		int antiguitatClients = 0;
+		boolean t = true;
 		Scanner s = new Scanner (System.in);
-		do {
+
+		while (t == true) {
 			System.out.print("Quant de temps estaràs estacionat (Minuts): ");
-			tempsEstacionament = s.nextDouble();
-			System.out.print("Quina es la longitud del teu vehicle (Metres: ");
+			tempsEstacionament = s.nextInt();
+			System.out.print("Quina es la longitud del teu vehicle (Metres): ");
 			longitudVehicle = s.nextDouble();
 			System.out.print("Quant portes venint a aquest aparcament?(Anys, xifra entera): ");
 			antiguitatClients = s.nextInt();
-			if (tempsEstacionament > longitudVehicle || tempsEstacionament  <= 0) {
+			t = false;
+			if (tempsEstacionament <= 0 || longitudVehicle <= 0 || antiguitatClients <= 0) {
 				System.out.println("No heu introduït els valors correctes.");
-				System.out.println("Recodeu que el 0 tampoc és un valor acceptat");
-				
 			}
-		} while (tempsEstacionament > longitudVehicle || tempsEstacionament  <= 0);
+			else {
+				t = false;
+			}
+		}
+		//dades de l'usuari 
+		System.out.printf("Vehicle 1 : (%d minuts d'estada, %.2f  m de longitud i client amb %d anys d'antiguitat)", tempsEstacionament, longitudVehicle, antiguitatClients);
 		s.close();
+		
 	}
 
 }
