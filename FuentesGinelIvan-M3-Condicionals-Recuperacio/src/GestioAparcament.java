e5073db4aeb5cffdcd8088cd037c3a1bb8567059
Kakashicolchon <jitsu.gokei@hotmail.com>
2017-11-25 15:41:23 +0100

    Release V1 (Sin opcional)

--- FuentesGinelIvan-M3-Condicionals-Recuperacio/src/GestioAparcament.java
+++ FuentesGinelIvan-M3-Condicionals-Recuperacio/src/GestioAparcament.java
@@ -10,12 +10,13 @@ public class GestioAparcament {
 		double antiguitatClients = 0;
 		boolean t = true;
 		double preuPerTemps, preuPerLong = 0;
-		double descompte;
+		double descompte = 0;
+		double preuFinal;
 		Scanner s = new Scanner (System.in);
 		//Preguntem les dades directament (canviar en el cas de la part opcional)
 		while (t == true) {
 			System.out.print("Quant de temps estaràs estacionat (Minuts): ");
-			tempsEstacionament = s.nextInt();
+			tempsEstacionament = s.nextDouble();
 			if (tempsEstacionament > 1440) {
 				//1440min = 24 hores
 				System.out.println("");
@@ -27,7 +28,7 @@ public class GestioAparcament {
 			System.out.print("Quina es la longitud del teu vehicle (Metres): ");
 			longitudVehicle = s.nextDouble();
 			System.out.print("Quant portes venint a aquest aparcament?(Anys, xifra entera): ");
-			antiguitatClients = s.nextInt();
+			antiguitatClients = s.nextDouble();
 			t = false;
 			if (tempsEstacionament <= 0 || longitudVehicle <= 0 || antiguitatClients <= 0) {
 				System.out.println("No heu introduït els valors correctes.");
@@ -43,9 +44,9 @@ public class GestioAparcament {
 			//Temps en el cas de 1 a 120 min
 			preuPerTemps = 0.04 * tempsEstacionament;
 		}
-		else if (tempsEstacionament <= 121 && tempsEstacionament >= 600) {
+		else if (tempsEstacionament >= 121 && tempsEstacionament <= 600) {
 			//Temps en el cas de 121 a 600
-			preuPerTemps = (0.04 * tempsEstacionament) + (0.027 * (tempsEstacionament-120)); 
+			preuPerTemps = (0.04 * 120) + (0.027 * (tempsEstacionament-120)); 
 		}
 		else {
 			//Temps en el cas de + de 600 (fins a 24 hores)
@@ -56,21 +57,47 @@ public class GestioAparcament {
 		if ( longitudVehicle > 4.5) {
 			preuPerLong = (preuPerTemps * 10) / 100; 
 		}
+		//Calculem si hi ha descompte i el seu valor.
 		if (antiguitatClients >= 5) {
 			descompte = ((preuPerLong + preuPerTemps) * 10) / 100;
 			
 		}
+		
+		//Calculem el preu final
+		preuFinal = preuPerTemps + preuPerLong - descompte;
 		 
 		
 		//mostrem els resultats a l'usuari
 		System.out.println("");
 		System.out.printf("Vehicle 1 : (%.2f minuts d'estada, %.2f  m de longitud i client amb %.2f anys d'antiguitat)\n", tempsEstacionament, longitudVehicle, antiguitatClients);
 		System.out.println("______________________________________________________________________________________");
-		System.out.println("Preu per temps                                   = " + " € x " + " minuts = " + "" );
-		System.out.println("Recàrrec per longitud del vehicle                = " + "");
-		System.out.println("Preu amb recàrrec per longitud                   = " + "");
-		System.out.println("Descompte per fidelitat                          = " + "");
-		System.out.println("Preu final                                       = " + "");
+		//Preu per temps
+		if (tempsEstacionament >= 120) {
+			System.out.printf("  >>  Preu per temps                                   = 0,04 x 120 minuts + 0,027 x %.2f = %.2f \n", tempsEstacionament, preuPerTemps);
+		}
+		else {
+			System.out.printf("  >>  Preu per temps                                   = 0,04 x %.2f minuts  = %.2f \n", tempsEstacionament, preuPerTemps);
+		}
+		//Preu per longitud
+		if (preuPerLong == 0) {
+			System.out.println("  >>  Recàrrec per longitud del vehicle(no)            = 0,00 €");
+			System.out.printf("  >>  Preu amb recàrrec per longitud                   = %.2f €", preuPerTemps);
+		}
+		else {
+			System.out.printf("  >>  Recàrrec per longitud del vehicle(si)            = %.2f € \n",preuPerLong );
+			double preuAmbRecarrec = preuPerTemps + preuPerLong;
+			System.out.printf("  >>  Preu amb recàrrec per longitud                   = %.2f + %.2f = %.2f € \n", preuPerTemps, preuPerLong,preuAmbRecarrec );
+		}
+		//Descompte fidelitat
+		if (antiguitatClients >= 5) {
+			System.out.printf("  >>  Descompte per fidelitat(si)                      = %.2f \n", descompte);
+		}
+		else {
+			System.out.println("  >>  Descompte per fidelitat(no)                   = 0,00 €");
+		}
+		
+		System.out.printf("  >>  Preu final                                       = %.2f \n", preuFinal);
+		System.out.println("______________________________________________________________________________________");
 		
 		
 		
