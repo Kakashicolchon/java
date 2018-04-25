96eabc5d694aba38f288281e09cda3fc92f26b7f
friday <friday@FRIDAY>
2018-04-25 11:00:19 +0200

    3 funciones (1 finished)

--- PrietoFuentesAntonioIvan-M3-EstacionsMeteorologiques/src/Exercici1.java
+++ PrietoFuentesAntonioIvan-M3-EstacionsMeteorologiques/src/Exercici1.java
@@ -27,10 +27,76 @@ public class Exercici1 {
 			return 0;
 	 }
 	
+	 /**
+	 * Mètode que llegeix les línies de l'arxiu que contenen dades sobre les estacions
+	 * meteorològiques, n'extreu el nom, la temperatura i la humitat relativa, i guarda
+	 * cada cosa dins la taula corresponent. Les taules que passem com a paràmetre són per
+	 * permetre el retorn de valors (paràmetres de sortida) i han de tenir totes tres la
+	 * mateixa mida (tantes posicions com línies de dades preveiem que tindrà el fitxer).
+	 * @param sc Objecte Scanner prèviament creat i associat a l'arxiu d'entrada. Se
+	 * suposa que d'aquest fitxer se n'ha llegit la primera línia, la que conté
+	 * el nombre de línies i, per tant, hi trobarem només les línies que contenen
+	 * les dades de les estacions meteorològiques.
+	 * @param n Taula de noms, creada amb tantes posicions com preveiem que caldran.
+	 * @param t Taula de temperatures, creada amb tantes posicions com preveiem que caldran.
+	 * @param h Taula d'humitats, creada amb tantes posicions com preveiem que caldran.
+	 * @return Valor de tipus enter que indica el nombre de línies de dades llegides. Com
+	 * a molt, tindrà el mateix valor que el length de les taules. Si les dades
+	 * s'acaben prematurament, retornarà un nombre de línies llegides menor que la
+	 * mida de les taules.
+	 */
+	 public static int llegeixDadesArxiu(Scanner sc, String[] n, double[] t, double[] h) {
+		 String linia;
+		 int contadorComes = 0;
+		 while (sc.hasNext() == true) {
+				linia = sc.nextLine();
+				for (int i = 0; i < linia.length(); i++) {
+					if (linia.charAt(i) == ',') {
+						contadorComes++;
+					}
+				}
+					
+			}
+	 }
 	
 	
 	
-	
+	 /**
+	 * Mètode al qual li passem una cadena CSV i ens retorna l'element que li demanem,
+	 * identificant l'element amb un nombre enter (1 és el primer element, 2 el segon, etc.).
+	 * @param cadenaCSV
+	  String que conté la cadena CSV de la qual en volem extreure
+	 *
+	  un element donat.
+	 * @param elementAExtreure Nombre de l'element que volem extreure de la cadena CSV.
+	 * @return
+	  String amb l'element extret de la cadena CSV. Si l'element
+	 *
+	  demanat no existeix, ens retorna null.
+	 */
+	 public static String extreuElementCSV(String cadenaCSV, int elementAExtreure) {
+		
+		 String elementCSV;
+		 int posicioInicialComa;
+		 int comptadorComes = 0;
+		 int posicioFinalComa;
+		 //coma = elementAExtreure -1
+		 //indexOF(",");
+		 //charAt();
+		 for (int i = 0; i < cadenaCSV.length(); i++) {
+			if (cadenaCSV.charAt(i) == ',') {
+				comptadorComes++;
+				posicioInicialComa = cadenaCSV.indexOf(",");
+				posicioFinalComa = cadenaCSV.indexOf(",", i+1);
+				if (elementAExtreure == comptadorComes-1) {
+					elementCSV = cadenaCSV.substring(posicioInicialComa, posicioFinalComa);
+					break;
+				}
+			}
+		}
+		 return elementCSV;
+	 }
+
 
 }
 
---
+++ PrietoFuentesAntonioIvan-M3-EstacionsMeteorologiques/src/pruebas1.java
@@ -0,0 +1,9 @@
+
+public class pruebas1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
